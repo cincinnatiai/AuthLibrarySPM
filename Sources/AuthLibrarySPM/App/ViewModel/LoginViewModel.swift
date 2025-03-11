3bcf92c228e78be9f0d894bf913f111ce4a591c5
Dionicio <dioniciocruzvelazquez@gmail.com>
2025-03-11 15:10:34 -0400

    Removing unnecessary code

--- Sources/AuthLibrarySPM/App/ViewModel/LoginViewModel.swift
+++ Sources/AuthLibrarySPM/App/ViewModel/LoginViewModel.swift
@@ -14,37 +14,37 @@ public class LoginViewModel: AuthViewModel {
     @Published public var password: String = ""
     @Published private var isFaceIDInProgress = false
     @Published public var authenticationError: String?
-    
+
     @Published public var isFaceIDEnabled: Bool {
         didSet {
             UserDefaults.standard.set(isFaceIDEnabled, forKey: "isFaceIDEnabled")
             UserDefaults.standard.synchronize()
         }
     }
-    
+
     private let faceIDAuthenticator = FaceIDAuthenticator()
     private let keychain: KeychainManager
-    
+
     public init(authManager: AuthManager, keychain: KeychainManager = KeychainManager()) {
         self.keychain = keychain
         self.isFaceIDEnabled = UserDefaults.standard.bool(forKey: "isFaceIDEnabled")
         super.init(authManager: authManager)
         loadCredentials()
-        
+
         Task {
             await tryAutoLogin()
         }
     }
-    
+
     private func tryAutoLogin() async {
         guard shouldAttemptAutoLogin() else { return }
         await attemptFaceIDLogin()
     }
-    
+
     public func attemptFaceIDLogin() async {
         guard !isFaceIDInProgress else { return }
         isFaceIDInProgress = true
-        
+
         do {
             let success = try await faceIDAuthenticator.authenticate()
             if success {
@@ -60,10 +60,9 @@ public class LoginViewModel: AuthViewModel {
         } catch {
             authenticationError = error.localizedDescription
         }
-        
         isFaceIDInProgress = false
     }
-    
+
     public func login() {
         if isFaceIDEnabled {
             Task {
@@ -75,36 +74,34 @@ public class LoginViewModel: AuthViewModel {
             keychain.set(email, key: "email")
         }
     }
-    
+
     public func signUp() {
         authManager.showSignUp()
     }
-    
+
     public func loadCredentials() {
         email = keychain.get(key: "email") ?? ""
     }
-    
+
     override public func clearErrorMessage() {
         super.clearErrorMessage()
     }
-    
+
     private func shouldAttemptAutoLogin() -> Bool {
         let hasLaunchedBefore = UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
         let hasLoggedOut = UserDefaults.standard.bool(forKey: "hasLoggedOut")
         let wasInBackground = UserDefaults.standard.bool(forKey: "wasInBackground")
-        
+
         if wasInBackground {
             UserDefaults.standard.set(false, forKey: "hasLaunchedBefore")
             UserDefaults.standard.set(false, forKey: "wasInBackground")
             return false
         }
-        
+
         if !hasLaunchedBefore {
             UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
             return true
         }
-        
         return !hasLoggedOut
     }
-    
 }
